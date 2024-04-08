6a7ea852907e5c95a011c2d2f20281caee727675
b.mousavi <b.mousavi@FFAZELI>
2024-04-08 12:01:11 +0330

    Add project files.

---
+++ VinProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using Emgu.CV;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+
+class Program
+{
+    static void Main()
+    {
+        Image<Bgr, byte> carCardImage = new Image<Bgr, byte>("C:\\vi.jpg");
+        Emgu.CV.OCR.Tesseract ocr = new Emgu.CV.OCR.Tesseract("tessdata", "eng", OcrEngineMode.Default);
+        ocr.SetImage(carCardImage);
+        ocr.Recognize();
+
+        string vinNumber = ocr.GetUTF8Text().Trim();
+        string searchTerm = "VIN: ";
+        int index = vinNumber.IndexOf(searchTerm);
+        if (index != -1)
+        {
+            int startIndex = index + searchTerm.Length;
+            string substring = vinNumber.Substring(startIndex, 15);
+            Console.WriteLine("VIN: " + substring);
+        }
+
+    }
+}
